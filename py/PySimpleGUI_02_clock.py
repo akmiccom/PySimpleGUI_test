a47ffb39808726ba73a8fb3b64a26c1f752501ab
makoto yaguchi <akmiccom@gmail.com>
2024-09-10 08:21:08 +0900

    update location

--- py/PySimpleGUI_02_clock.py
+++ py/PySimpleGUI_02_clock.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # PySimpleGUI Watch
+# # PySimpleGUI Clock
 # - PySimpleGUI を使って時計ウィジェットを作成
-# - 
+# - フォント選択設定 [フォント選択設定 PySimpleGUI-github](https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Font_Previewer.py)
 
-# In[6]:
+# In[1]:
 
 
 import PySimpleGUI as sg
 from datetime import datetime
 from pyautogui import size
 
+
+# fonts = sg.Text.fonts_installed_list()
 
 layout = [
     [
@@ -29,13 +31,19 @@ layout = [
             key="-date-",
         ),
     ],
+    # [
+    #     sg.Listbox(
+    #         # fonts,
+    #         size=(30, 20),
+    #         change_submits=True, key='-list-')
+    # ],
 ]
 
 window = sg.Window(
     title="clock",
     layout=layout,
-    size=(400, 200),
-    location=(size()[0] - 395, size()[1] - 275),
+    # size=(400, 200),
+    location=(size()[0] - 350, size()[1] - 380),
     transparent_color=sg.theme_background_color(),
     no_titlebar=True,
     right_click_menu=["menu", ["Exit", "!Properties"]],
